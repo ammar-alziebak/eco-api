4eccc35db123a5bb60b6ca10e246d5ce739eaab3
ammar alziebak <ammar.alziebak@hotmail.com>
2021-07-01 12:46:16 +0800

    category count

--- api/service/categories.js
+++ api/service/categories.js
@@ -4,7 +4,10 @@ const { v1: uuidv1 } = require('uuid');
 exports.get_all_categories = (data, callBack) => {
 
     pool.query({
-        sql: `SELECT categories.* FROM categories;`,
+        sql: `SELECT categories.*,
+                     (SELECT COUNT(*) FROM product WHERE product.categoryId = categories.id)+
+                     (SELECT COUNT(*) FROM product WHERE product.categoryId IN (SELECT c.id FROM categories c WHERE c.parentId = categories.id )) count
+              FROM categories;`,
     }, (error, result, fields) => {
 
         if (error) {
